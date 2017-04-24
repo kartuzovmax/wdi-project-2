84222b20286df1b4d7cd763d1f3970fdb46a808d
kartuzovmax <kartuzovmax@hotmail.com>
2017-04-24 11:10:16 +0500

    Implemented Authentication

--- config/routes.js
+++ config/routes.js
@@ -5,6 +5,8 @@ router.get('/', (req, res) => res.render('statics/home'));
 
 // Controllers
 const statics = require('../controllers/statics');
+const registrations = require('../controllers/registrations');
+const sessions = require('../controllers/sessions');
 
 // function secureRoute(req, res, next) {
 //   if (!req.session.userId) {
@@ -25,6 +27,14 @@ router.route('/statics/home')
       .get(statics.static);
 router.route('/statics/user')
       .get(statics.user);
+router.route('/register')
+      .get(registrations.new)
+      .post(registrations.create);
+router.route('/login')
+      .get(sessions.new)
+      .post(sessions.create);
+router.route('/logout')
+      .get(sessions.delete);
 
 
 module.exports = router;
--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const expressLayouts  = require('express-ejs-layouts');
 const bodyParser      = require('body-parser');
 const mongoose        = require('mongoose');
 const methodOverride  = require('method-override');
+const flash             = require('express-flash');
+const session           = require('express-session');
+
+const app             = express();
+
+const User            = require('./models/user');
 const env             = require('./config/env');
 const router          = require('./config/routes');
-const app             = express();
 
 mongoose.connect(env.db);
 
@@ -22,6 +27,37 @@ app.use(methodOverride((req) => {
     return method;
   }
 }));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'ssh it\'s a secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(flash());
+
+app.use((req, res, next) => {
+  if (!req.session.userId) return next();
+
+  User
+  .findById(req.session.userId)
+  .exec()
+  .then((user) => {
+    if(!user) {
+      return req.session.regenerate(() => {
+        res.redirect('/');
+      });
+    }
+
+    // Re-assign the session id for good measure
+    req.session.userId = user._id;
+
+    res.locals.user = user;
+    res.locals.isLoggedIn = true;
+
+    next();
+  });
+});
 
 app.use(router);
 
